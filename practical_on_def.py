7c3efc3e4e99b857b93bad2b0fa83d9c2abb8bf9
enenchedominion2008 <enenchedominion@gmail.coom>
2026-09-08 22:48:32 +0100

    i had made some practical on the def functions i was having some issues with it so i have done a little calculator project on it althoug i have not peerfected all the edge cases

---
+++ practical_on_def.py
@@ -0,0 +1,55 @@
+def add(a, b) :
+    return a + b
+def sub(a, b) :
+    return a - b
+def div(a, b) :
+    return a / b
+def mul(a, b) :
+    return a * b
+
+method = ""
+
+while method != "exit" :
+
+    method = input("choses your method of calculation \n add \n sub \n div \n mul \n >>> : ")
+
+    if method == "add":
+
+        first_number = int(input("input your first number >> "))
+
+        second_number = int(input("input your second number >> "))
+
+        result = add(first_number, second_number)
+        
+        print(f"result >> {result} ")
+
+    elif method == "sub" :
+
+        first_number = int(input("input your first number >> "))
+
+        second_number = int(input("input your second number >> "))
+        
+        result = sub(first_number, second_number)
+        
+        print(f"result >> {result} ")
+
+    elif method == "div" :
+
+        first_number = int(input("input your first number >> "))
+
+        second_number = int(input("input your second number >> "))
+
+        result = div(first_number, second_number)
+
+        print(f"result >> {result} ")
+
+    elif method == "mul" :
+
+        first_number = int(input("input your first number >> "))
+
+        second_number = int(input("input your second number >> "))
+
+        result = mul(first_number, second_number)
+
+        print(f"result >> {result} ")
+
