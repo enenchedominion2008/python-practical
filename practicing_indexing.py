17eee2426a5c0e839c4adb835cd65788bec8f06b
enenchedominion2008 <enenchedominion@gmail.com>
2026-09-12 23:34:02 +0000

    i am doing some python excercises

--- practicing_indexing.py
+++ practicing_indexing.py
@@ -9,9 +9,27 @@ name = "Dominion"
 # print(ai[1:])
 # print(ai[-3:])
 
-name = "  Dominion  "
+# name = "  Dominion  "
 
-print(name.upper())      # "  DOMINION  "  — all uppercase
-print(name.lower())      # "  dominion  "  — all lowercase
-print(name.strip())      # "Dominion"      — removes leading/trailing whitespace
-print(name.replace("o", "0"))   # "  D0min10n  " — replaces every match
+# print(name.upper())      # "  DOMINION  "  — all uppercase
+# print(name.lower())      # "  dominion  "  — all lowercase
+# print(name.strip())      # "Dominion"      — removes leading/trailing whitespace
+# print(name.replace("o", "0"))   # "  D0min10n  " — replaces every match
+
+# name = "  HELLo WORLD       "
+# clean_name = name.strip().lower().replace("world","people")
+# print(clean_name)   
+
+#excercise 1 
+# working on reversing strings 
+# words = "i love python"
+# words = words.split() 
+# reverse = words[::-1]
+# reverse = " ".join(reverse)
+# print(reverse)
+
+# excercise 2
+#counting the leanght of a raw sentence 
+word = "hello how are you"
+word = word.split()
+print(len(word))
