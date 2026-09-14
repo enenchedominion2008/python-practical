54e990974fee0d3f5f6e265476c1a0be5d37a23e
enenchedominion2008 <enenchedominion@gmail.com>
2026-09-15 00:55:36 +0100

    Add files via upload
    i did the work on a shoping listing stuff

--- shoping_list.py
+++ shoping_list.py
@@ -1,4 +1,8 @@
-listing = ""
-while listing != "exit" :
+shoping_list = []
+while True :
     listing = input("input your what you want to buy : ")
-    
+
+    if listing == "done":
+        break
+    shoping_list.append(listing)
+    print(f"here is your order {shoping_list}")
