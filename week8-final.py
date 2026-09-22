52668f378df3e2e01c3f8b2591162dbc5879e3df
enenchedominion2008 <enenchedominion@gmail.com>
2026-09-22 10:47:21 +0100

    now is the complete pracical for file handling

--- week8-final.py
+++ week8-final.py
@@ -5,28 +5,53 @@ while True :
     print("(2) view student")
     print("(3) search")
     print("(4) exit")
+
     option = input("what do you want to do >> ")
+
     if option == "1" :
+
         while option != "done" :
+
             name = input("add student names type or type 'done' to exit >> ")
+
             if name == "done" :
+
                 break
             with open("student.txt","a") as file :
+
                 file.write(name+"\n")
+
     elif option == "2" :
+
         with open("student.txt","r") as file :
+
             for line in file :
+
                 print(line,end="")
+
     elif option == "3" :
+
+
         search = input("search student >> ")
+
+        found = False
+
         with open("student.txt","r") as file :
+           
            for line in file :
+               
                if line.strip() == search:
-                   print("student <<"+search+">>found") 
+                   
+                   found = True 
+                   break 
+        if found :
+            print("student <<"+search+">> found")
+        else :
+            print("student <<"+search+">> not found")
     elif option == "4" :
         break
     else :
-        print("🚫🚫🚫 that is not a correct option \n please read well before \n preforming any action 🚫🚫🚫")
+        print(f"🚫🚫🚫  please \n<<<({option})>>\n is not a correct option \n please read well before \n preforming any action 🚫🚫🚫")
 
 print("work saved✅")
 
